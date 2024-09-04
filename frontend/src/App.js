cb7142665c3d4be9834ba306c9f9994f29aab8ee
BuraYu <burak.yueksel@hotmail.com>
2024-09-04 15:15:29 +0200

    add scraper input

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>scraper</h1>
+    <div className="app-container">
+      <div className="input-container">
+        <label for="websiteInput"></label>
+        <input
+          className="input"
+          type="text"
+          id="websiteInput"
+          placeholder="enter URL here"
+        />
+
+        <button className="button-scrape">Scrape</button>
+      </div>
     </div>
   );
 }
